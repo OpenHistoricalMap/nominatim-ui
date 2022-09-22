9e866b2747bd354e9e28636d0a1f00fb22e3ecf1
Rub21 <ruben@developmentseed.org>
2022-09-22 16:57:50 -0500

    Updates

---
+++ theme/config.theme.js
@@ -0,0 +1,5 @@
+// Example file how to overwrite defaults from config.default.js
+// The file should be named 'config.theme.js'
+
+Nominatim_Config.Nominatim_API_Endpoint = 'https://nominatim-staging.openhistoricalmap.org/';
+Nominatim_Config.Page_Title = 'My Server demo;
