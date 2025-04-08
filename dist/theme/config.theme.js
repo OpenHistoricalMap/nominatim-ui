ecff03a79881a5926bebcf60a8439d4147a51980
Rub21 <rub2106@gmail.com>
2025-04-08 08:25:24 -0500

    Update Map_Tile_Attribution

--- dist/theme/config.theme.js
+++ dist/theme/config.theme.js
@@ -1,1 +1,2 @@
 Nominatim_Config.Page_Title = 'OpenHistoricalMap';
+Nominatim_Config.Map_Tile_Attribution = '<a href="https://www.openhistoricalmap.org/copyright">OpenHistoricalMap contributors</a>'
